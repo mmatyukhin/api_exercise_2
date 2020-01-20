82d249267b95e5f96df2411ca0e63e1dd33a584e
Mikhail Matyukhin <itsmatyukhin@gmail.com>
2020-01-21 00:57:31 +0300

    Set up simple authorization

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
+gem 'jwt'
 gem 'will_paginate', '~> 3.1.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,12 @@
 class ApiController < ApplicationController
-  before_action :set_default_format
+  VERY_SECURE_STRING = 'secure string'
+  VERY_SECURE_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.InNlY3VyZSBzdHJpbmci._EZH5dX9UCDY_L1NbOv4-wpyPrUly-XMfWNLqn2fDjk'
+  before_action :set_default_format, :authenticate_request!
+
+  def authenticate_request!
+    token = request.headers['Authorization']
+    JWT.decode(token, 'secret') == VERY_SECURE_STRING
+  end
 
   private
 
